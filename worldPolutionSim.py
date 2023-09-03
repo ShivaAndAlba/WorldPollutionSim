c5e3c661983818f732f2b77b8ecf36fb1fb9a7dd
ShivaAndAlba <goshayan@gmail.com>
2023-09-03 12:58:18 +0300

    code cleanup
    change class worlpollutinmodel to singleton class
    aligning to pep8 coding conventions

--- worldPolutionSim.py
+++ worldPolutionSim.py
@@ -1,82 +1,118 @@
-from  random import *
+from random import *
 from re import RegexFlag, findall
-from tkinter import *
+import tkinter as tk
 from dataclasses import dataclass
 
 
 worldMap = [
-['S', 'S', 'S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'F', 'M', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'F', 'C', 'S', 'F', 'F', 'C', 'F', 'C', 'C', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'F', 'F', 'C', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'F', 'S', 'S', 'F', 'F', 'C', 'F', 'C', 'C', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'D', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'F', 'F', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'D', 'D', 'S', 'S', 'F', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'F', 'M', 'C', 'M', 'C', 'F', 'C', 'S', 'S', 'S', 'S', 'F', 'C', 'F', 'F', 'F', 'F', 'F', 'D', 'F', 'F', 'F', 'F', 'F', 'F', 'D', 'D', 'D', 'D', 'S', 'S', 'F', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'C', 'F', 'M', 'M', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'F', 'S', 'F', 'F', 'D', 'D', 'D', 'D', 'F', 'F', 'F', 'F', 'D', 'D', 'D', 'D', 'F', 'S', 'F', 'F', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'S', 'C', 'D', 'S', 'F', 'F', 'F', 'F', 'F', 'D', 'F', 'F', 'S', 'S', 'F', 'C', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'C', 'C', 'D', 'S', 'F', 'F', 'F', 'S', 'F', 'F', 'C', 'S', 'S', 'S', 'F', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'F', 'C', 'F', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'F', 'S', 'S', 'S', 'C', 'S', 'F', 'S', 'S', 'F', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'M', 'C', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'S', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'C', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'S', 'S', 'M', 'C', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'D', 'D', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'I', 'S', 'M', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'C', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'C', 'C', 'F', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'S', 'S', 'S', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
-['I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I']]
+    ['S', 'S', 'S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'I', 'I',
+        'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'I', 'I',
+        'I', 'I', 'I', 'I', 'I', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F',
+        'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'F', 'M', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'F', 'C', 'S', 'F', 'F', 'C',
+        'F', 'C', 'C', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'F', 'F', 'C', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'F', 'S', 'S', 'F', 'F', 'C',
+        'F', 'C', 'C', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'D', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'F', 'F', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'F',
+        'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'D', 'D', 'S', 'S', 'F', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'F', 'M', 'C', 'M', 'C', 'F', 'C', 'S', 'S', 'S', 'S', 'F', 'C', 'F', 'F', 'F',
+        'F', 'F', 'D', 'F', 'F', 'F', 'F', 'F', 'F', 'D', 'D', 'D', 'D', 'S', 'S', 'F', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'C', 'F', 'M', 'M', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'F', 'S', 'F', 'F',
+        'D', 'D', 'D', 'D', 'F', 'F', 'F', 'F', 'D', 'D', 'D', 'D', 'F', 'S', 'F', 'F', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F',
+        'S', 'C', 'D', 'S', 'F', 'F', 'F', 'F', 'F', 'D', 'F', 'F', 'S', 'S', 'F', 'C', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D',
+        'C', 'C', 'D', 'S', 'F', 'F', 'F', 'S', 'F', 'F', 'C', 'S', 'S', 'S', 'F', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'F', 'C', 'F', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'D',
+        'D', 'S', 'S', 'S', 'S', 'F', 'S', 'S', 'S', 'C', 'S', 'F', 'S', 'S', 'F', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'M', 'C', 'F', 'F', 'F', 'F', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'D',
+        'D', 'D', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'S', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'C', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F',
+        'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F', 'F',
+        'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'M', 'F', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F',
+        'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'M', 'C', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'F',
+        'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'F', 'D', 'D', 'D', 'D', 'D', 'D', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'I', 'S', 'M', 'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'C',
+        'F', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'D', 'D', 'C', 'C', 'F', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S',
+        'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'I',
+        'I', 'I', 'I', 'S', 'S', 'S', 'S', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I']]
 
 
 @dataclass
 class WorldDiementions:
     length: int
-    width: int 
-    cellSize: int
+    width: int
+    cell_size: int
 
-    def getCanvasSizeLength(self):
-        return self.length*self.cellSize
-    
-    def getCanvasSizeWidth(self):
-        return self.width*self.cellSize
+    def get_cell_length(self):
+        """returns cell length"""
+        return self.length*self.cell_size
+
+    def get_cell_width(self):
+        """returns cell width"""
+        return self.width*self.cell_size
+
 
 class GlobalWarmingModel():
-    def __init__(self, worldDimention):
-        self.worldDimention = worldDimention
-        self.grid = Grid(self.worldDimention)
-        self.guiWin = self.createGui()
+    """singelton class to handle all model functionality"""
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(GlobalWarmingModel, cls).__new__(cls)
+        return cls.instance
 
-    def createGui(self):
-        self.root = Tk()
+    def __init__(self, world_dimention):
+        self.world_dimention = world_dimention
+        self.grid = Grid(self.world_dimention)
+
+        """ creates gui via tkinter"""
+        self.root = tk.Tk()
         self.title = self.root.title("Air Polution Model - Ex. 11")
-        self.label = Label(self.root)
+        self.label = tk.Label(self.root)
         self.label.pack()
-        self.canvas = Canvas(self.root, height=self.worldDimention.getCanvasSizeWidth(), width=self.worldDimention.getCanvasSizeLength())
-        self.canvas.pack()   
-        self.addCellsToCanvas() 
+        self.canvas = tk.Canvas(self.root,
+                                height=self.world_dimention.get_cell_width(),
+                                width=self.world_dimention.get_cell_length())
+        self.canvas.pack()
+        self.insert_cell()
         self.root.mainloop()
-    
-    def addCellsToCanvas(self):
-        for y in range(self.worldDimention.width):
-            for x in range(self.worldDimention.length):
+
+    def insert_cell(self):
+        """create and insert cells to canvas"""
+        for y in range(self.world_dimention.width):
+            for x in range(self.world_dimention.length):
                 cell = self.grid.getCellAt(y, x)
-                cell.setCanvasId(self.canvas.create_rectangle(x*self.worldDimention.cellSize,
-                                                        y*self.worldDimention.cellSize,
-                                                        (x+1)*self.worldDimention.cellSize,
-                                                        (y+1)*self.worldDimention.cellSize,
-                                                        fill=cell.getColor()))
-                cell.setCanvasText(self.canvas.create_text((x + 0.5) * worldDiemention.cellSize,
-                                                        (y + 0.5) * worldDiemention.cellSize,
-                                                        text=str(cell.getTemprature())))
+                cell.setCanvasId(
+                    self.canvas.create_rectangle(
+                        x*self.world_dimention.cell_size,
+                        y*self.world_dimention.cell_size,
+                        (x+1)*self.world_dimention.cell_size,
+                        (y+1)*self.world_dimention.cell_size,
+                        fill=cell.getColor()))
+                cell.setCanvasText(
+                    self.canvas.create_text(
+                        (x + 0.5) * worldDiemention.cell_size,
+                        (y + 0.5) *
+                        worldDiemention.cell_size,
+                        text=str(cell.getTemprature())))
+
 
 class Cell():
     def __init__(self, posY, posX) -> None:
         self.posX = posX
-        self.posY = posY 
+        self.posY = posY
         self.cellType: str
         self.canvasText: Canvas
         self.canvasId: Canvas
         self.color: str
-        
+
         self.norhNeighbor: Cell
         self.southNeighbor: Cell
         self.westNeighbor: Cell
@@ -120,10 +156,10 @@ class Cell():
             self.airPolution = 1
         else:
             self.airPolution = 0
-    
+
         self.windDirection = choice(['N', 'S', 'W', 'E'])
         self.windSpeed = randint(0, 30)
-    
+
         self.clouds = choice([True, False])
         if self.clouds:
             if self.cellType == 'F':
@@ -139,7 +175,6 @@ class Cell():
             elif self.cellType == 'M':
                 self.color = '#a16707'
 
-    
     def getTemprature(self):
         return self.temprature
 
@@ -148,7 +183,7 @@ class Cell():
 
     def getWind(self):
         return self.windDirection, self.windSpeed
-    
+
     def getClouds(self):
         return self.clouds
 
@@ -160,7 +195,7 @@ class Cell():
 
     def getCanvasId(self):
         return self.canvasId
-    
+
     def setCanvasText(self, text: str):
         self.canvasText = text
 
@@ -169,15 +204,15 @@ class Cell():
 
     def getXPosition(self):
         return self.posX
-    
+
     def getYPosition(self):
         return self.posY
 
     def getNorthNeighbor(self):
-       return self.norhNeighbor  
+        return self.norhNeighbor
 
     def setNorthNeighbor(self, neighbor):
-       self.norhNeighbor = neighbor 
+        self.norhNeighbor = neighbor
 
     def getSouthNeighbor(self):
         return self.southNeighbor
@@ -189,7 +224,7 @@ class Cell():
         return self.westNeighbor
 
     def setWestNeighbor(self, neighbor):
-        self.westNeighbor = neighbor 
+        self.westNeighbor = neighbor
 
     def getEastNeighbor(self):
         return self.eastNeighbor
@@ -197,10 +232,12 @@ class Cell():
     def setEastNeighbor(self, neighbor):
         self.eastNeighbor = neighbor
 
+
 class Grid():
     def __init__(self, dimentions: WorldDiementions) -> None:
         self.dimentions = dimentions
-        self.grid = [[self.createCellAt(y,x) for x in range(dimentions.length)] for y in range(dimentions.width)]
+        self.grid = [[self.createCellAt(y, x) for x in range(
+            dimentions.length)] for y in range(dimentions.width)]
         self.updateCellsNeighbors()
 
     def createCellAt(self, y, x):
@@ -212,10 +249,11 @@ class Grid():
     def updateCellsNeighbors(self):
         for y in range(self.dimentions.width):
             for x in range(self.dimentions.length):
-                currCell = self.getCellAt(y,x)                
-                
+                currCell = self.getCellAt(y, x)
+
                 if self.cellNorthBound(currCell):
-                    currCell.setNorthNeighbor(self.getCellAt(self.dimentions.width - 1, x))
+                    currCell.setNorthNeighbor(
+                        self.getCellAt(self.dimentions.width - 1, x))
                 else:
                     currCell.setNorthNeighbor(self.getCellAt(y-1, x))
 
@@ -225,7 +263,8 @@ class Grid():
                     currCell.setSouthNeighbor(self.getCellAt(y+1, x))
 
                 if self.cellWestBound(currCell):
-                    currCell.setWestNeighbor(self.getCellAt(y, self.dimentions.length - 1))
+                    currCell.setWestNeighbor(
+                        self.getCellAt(y, self.dimentions.length - 1))
                 else:
                     currCell.setWestNeighbor(self.getCellAt(y, x-1))
 
@@ -234,7 +273,6 @@ class Grid():
                 else:
                     currCell.setEastNeighbor(self.getCellAt(y, x+1))
 
-
     def cellNorthBound(self, cell: Cell):
         return cell.getYPosition == 0
 
@@ -242,12 +280,12 @@ class Grid():
         return cell.getYPosition() > self.dimentions.width - 2
 
     def cellWestBound(self, cell: Cell):
-        return cell.getXPosition() == 0 
+        return cell.getXPosition() == 0
 
     def cellEastBound(self, cell: Cell):
         return cell.getXPosition() > self.dimentions.length - 2
 
 
 if __name__ == "__main__":
-    worldDiemention = WorldDiementions(length=40, width=20, cellSize=20) 
+    worldDiemention = WorldDiementions(length=40, width=20, cell_size=20)
     GWModel = GlobalWarmingModel(worldDiemention)
